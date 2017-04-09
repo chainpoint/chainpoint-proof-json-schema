495732a06654615f2b2d4c5716c59f769a99d3c2
Glenn Rempe <glenn@tierion.com>
2017-04-09 14:59:24 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Chainpoint Proof v3 JSON Schema Validator
+
+A simple npm package to validate a Chainpoint v3 Proof, in JSON format, against a JSON Schema (Draft 04).
+
+## Try it out
+
+Test out the schema validation with a sample proof,
+or one of your own.
+
+[https://chainpoint.org/chainpoint-proof-json-schema/](https://chainpoint.org/chainpoint-proof-json-schema/)
+
+
+## Learn More About JSON Schema:
+
+https://spacetelescope.github.io/understanding-json-schema/index.html
+
+## Development
+
+### Test
+
+```
+yarn test
+```
+
+### Browser Packaging
+
+Will store a browserify bundle to `docs/bundle.js` which can be used
+directly within the `<script>` tags in an HTML page.
+
+```
+yarn run bundle
+```
---
+++ index.js
@@ -0,0 +1,159 @@
+'use strict'
+
+const chainpointSchemaV3 = {
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'additionalProperties': false,
+  'definitions': {
+    'branch': {
+      'additionalProperties': false,
+      'properties': {
+        'label': {
+          'description': 'An aritrary text branch label. Can contain up to 64 letters, numbers, hyphen, underscore, or period characters.',
+          'pattern': '^[a-zA-Z0-9-_\\.]*$',
+          'title': 'The Label Schema',
+          'type': 'string',
+          'minLength': 0,
+          'maxLength': 64
+        },
+        'branches': {
+          'items': {
+            '$ref': '#/definitions/branch'
+          },
+          'type': 'array',
+          'uniqueItems': true
+        },
+        'ops': {
+          'items': {
+            '$ref': '#/definitions/operation'
+          },
+          'type': 'array'
+        }
+      },
+      'required': ['ops'],
+      'type': 'object'
+    },
+    'anchor': {
+      'additionalProperties': false,
+      'properties': {
+        'type': {
+          'description': 'A trust anchor',
+          'title': 'Concatenate value with left side of previous value',
+          'type': 'string',
+          'enum': ['cal', 'eth', 'btc']
+        },
+        'anchor_id': {
+          'description': 'An identifier used to look up embedded anchor data. e.g. a Bitcoin transaction or block ID.',
+          'title': 'A service specific unique ID for this anchor',
+          'type': 'string',
+          'minLength': 1,
+          'maxLength': 512
+        },
+        'uris': {
+          'items': {
+            'description': "A URI used to lookup and retrieve the exact hash resource required to validate this anchor. The URI MUST return only a Hexadecimal hash value as a string. The URI MUST also contain the current 'anchor_id' value to lookup the URI resource. This strict requirement is to allow automated clients to retrieve and validate intermediate hashes when verifying a proof. The body value returned by the URI MUST be of even length and match the regex /^[a-fA-F0-9]+$/.",
+            'title': 'A URI for retrieving a hash value for this item',
+            'type': 'string',
+            'format': 'uri',
+            'minLength': 1,
+            'maxLength': 512
+          },
+          'type': 'array',
+          'uniqueItems': true
+        }
+      },
+      'required': ['type', 'anchor_id'],
+      'type': 'object'
+    },
+    'operation': {
+      'additionalProperties': false,
+      'properties': {
+        'l': {
+          'description': 'Concatenate the byte array value of this property to the left of the prior state of the hash (value|prior_hash).',
+          'title': 'Concatenate value with left side of previous value',
+          'type': 'string',
+          'minLength': 1,
+          'maxLength': 512
+        },
+        'r': {
+          'description': 'Concatenate the byte array value of this property to the right of the prior state of the hash (prior_hash|value).',
+          'title': 'Concatenate value with right side of previous value',
+          'type': 'string',
+          'minLength': 1,
+          'maxLength': 512
+        },
+        'op': {
+          'description': "A hashing operation from the SHA2 or SHA3 families of hash functions to apply to a left or right operation hash value. The special value of 'sha-256-x2' performs a 'sha-256' twice in a row.",
+          'title': 'The hashing operation to apply to a left or right hash',
+          'type': 'string',
+          'enum': ['sha-224', 'sha-256', 'sha-384', 'sha-512', 'sha3-224', 'sha3-256', 'sha3-384', 'sha3-512', 'sha-256-x2']
+        },
+        'anchors': {
+          'items': {
+            '$ref': '#/definitions/anchor'
+          },
+          'type': 'array',
+          'uniqueItems': true
+        }
+      },
+      'type': 'object'
+    }
+  },
+  'description': 'This document contains a schema for validating an instance of a Chainpoint v3 Proof.',
+  'id': 'http://example.com/example.json',
+  'properties': {
+    '@context': {
+      'default': 'https://w3id.org/chainpoint/v3',
+      'description': 'A registered JSON-LD context URI for this document type',
+      'title': 'The JSON-LD @context',
+      'type': 'string',
+      'enum': ['https://w3id.org/chainpoint/v3']
+    },
+    'type': {
+      'default': 'Chainpoint',
+      'description': 'The JSON-LD Type',
+      'title': 'The JSON-LD Type',
+      'type': 'string',
+      'enum': ['Chainpoint']
+    },
+    'hash': {
+      'description': 'The even length Hexadecimal output of a cryptographic one-way hash function representing the data to be anchored.',
+      'pattern': '^[a-fA-F0-9]{40,128}$',
+      'title': 'The hash to be anchored',
+      'type': 'string'
+    },
+    'hash_id': {
+      'description': 'The Type 1 (timestamp) UUID used to identify and track a hash or retrieve a Chainpoint proof',
+      'pattern': '^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$',
+      'title': 'A Type 1 (timestamp) UUID that identifies a hash',
+      'type': 'string'
+    },
+    'hash_submitted_at': {
+      'default': '2017-03-23T11:30:33Z',
+      'description': 'The timestamp, in ISO8601 form, extracted from the hash_id that represents the time the hash was submitted for anchoring.',
+      'pattern': '^\\d{4}(-\\d\\d(-\\d\\d(T\\d\\d:\\d\\d(:\\d\\d)?(\\.\\d+)?(([+-]\\d\\d:\\d\\d)|Z)?)?)?)?$',
+      'title': 'An ISO8601 timestamp, extracted from the hash_id',
+      'type': 'string'
+    },
+    'branches': {
+      'items': {
+        '$ref': '#/definitions/branch'
+      },
+      'type': 'array',
+      'uniqueItems': true
+    }
+  },
+  'required': ['@context', 'type', 'hash', 'hash_id', 'hash_submitted_at', 'branches'],
+  'title': 'Chainpoint v3 JSON Schema.',
+  'type': 'object'
+}
+
+const validator = require('is-my-json-valid')
+const validateSchema = validator(chainpointSchemaV3, { verbose: true })
+
+exports.validate = function (proof) {
+  // Return both in a single object since the validator
+  // actually mutates itself to hold the errors after
+  // it is first run. The original call to 'validate'
+  // will only return a Boolean.
+  return {valid: validateSchema(proof), errors: validateSchema.errors}
+}
